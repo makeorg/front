a7d429dfddf18eb791b265fe7c1d117f20a1564f
Philippe de Araujo <pa@make.org>
2021-07-19 10:32:11 +0200

    chore(transverse): change log params order

--- server/logger.js
+++ server/logger.js
@@ -89,8 +89,7 @@ const getLogFormat = sourceMapReplace => {
         : data
     );
 
-    // eslint-disable-next-line max-len
-    return `infoTime:${infoTime}  infoLevel:${infoLevel} infoLabel:${infoLabel} message:${message} stackTrace:${infoStack}`;
+    return `infoTime:${infoTime} infoLabel:${infoLabel} infoLevel:${infoLevel} message:${message} stackTrace:${infoStack}`;
   });
 };
 
