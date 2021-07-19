174d2dcf8147c07bb1515fbd84b358186ffa1fe0
Philippe de Araujo <pa@make.org>
2021-07-19 10:25:02 +0200

    fix(proposal): avoid error when use navigator back from propose panel
    closes #MP-2107

--- client/features/proposal/Submit/Journey.js
+++ client/features/proposal/Submit/Journey.js
@@ -1,5 +1,5 @@
 // & flow
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   trackClickBackProposals,
   trackDisplayProposalSubmitValidation,
@@ -14,6 +14,8 @@ import { ProposalService } from 'Shared/services/Proposal';
 import { modalShowProposalSuccess } from 'Shared/store/actions/modal';
 import { proposeSuccess } from 'Shared/store/actions/proposal';
 import { getLocalizedBaitText } from 'Shared/helpers/proposal';
+import { DEFAULT_LANGUAGE } from 'Shared/constants/config';
+import { useLocation } from 'react-router';
 import { ProposalForm } from './Form';
 import { ProposalAuthentication } from './Authentication';
 
@@ -24,18 +26,22 @@ const steps = {
 
 export const ProposalJourney = () => {
   const dispatch = useDispatch();
+  const location = useLocation();
+  const pathname = useRef(location.pathname);
+
   const { isLoggedIn } = useSelector((state: StateRoot) =>
     selectAuthentication(state)
   );
 
   const { question } = useSelector(
-    state => state.questions[state.currentQuestion]
+    state => state.questions[state.currentQuestion] || {}
   );
   const [proposalContent, setProposalContent] = useState('');
   const [proposalStep, setProposalStep] = useState('form');
   const [waiting, setWaiting] = useState(false);
+
   const baitText = getLocalizedBaitText(
-    question?.language,
+    question?.language || DEFAULT_LANGUAGE,
     question?.questionId
   );
 
@@ -83,6 +89,18 @@ export const ProposalJourney = () => {
     }
     setProposalStep(steps.AUTHENTICATION_STEP);
   };
+
+  useEffect(() => {
+    if (pathname.current !== location.pathname) {
+      dispatch(closePanel());
+      dispatch(removePanelContent());
+      pathname.current = location.pathname;
+    }
+  }, [location.pathname, dispatch]);
+
+  if (!question) {
+    return null;
+  }
 
   if (proposalStep === steps.AUTHENTICATION_STEP) {
     return (
